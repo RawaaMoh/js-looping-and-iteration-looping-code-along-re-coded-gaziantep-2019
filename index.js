5976add247fdadfbad3f25b1774caf8defa1a76a
Rüya Rava Rajab <rawaa.rajab@gmail.com>
2019-07-10 16:30:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cards = ["Ada", "Brendan", "Ali"];
  
 function writeCards(cards) {
   for (let i = 0; i < cards.length; i++) {
-    console.log(`Thank you,  ${cards[i]}, and added a bow!`);
+    console.log(`Thank you,  ${cards[i]}, for the wonderful birthday gift.`);
   }
  
   return cards;
