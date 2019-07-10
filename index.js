868900807d3c362afd19311cd25c9d0070c1f18f
Rüya Rava Rajab <rawaa.rajab@gmail.com>
2019-07-10 16:26:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const gifts = ["teddy bear", "drone", "doll"];
  
-function wrapGifts(gifts) {
+function wrıteCards(gifts) {
   for (let i = 0; i < gifts.length; i++) {
     console.log(`Wrapped ${gifts[i]} and added a bow!`);
   }
